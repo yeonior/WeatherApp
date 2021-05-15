1f859b549b16d87e3ff4113c4ae8c0259961b66c
Rus <de.mr44@gmail.com>
2021-05-15 23:29:32 +0500

    Added outlets and actions

--- WeatherApp/Extensions/ViewController+alertController.swift
+++ WeatherApp/Extensions/ViewController+alertController.swift
@@ -5,8 +5,32 @@
 //  Created by ruslan on 14.05.2021.
 //
 
-import Foundation
+import UIKit
 
 extension ViewController {
+    
+    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: style)
+        ac.addTextField { tf in
+            let cities = ["New York", "London", "Moscow", "Tokyo", "Prague"]
+            tf.placeholder = cities.randomElement()
+        }
+        
+        let searchAction = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = ac.textFields?.first
+            guard let cityName = textField?.text else { return }
+            if cityName != "" {
+                print("\(cityName)")
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        
+        ac.addAction(searchAction)
+        ac.addAction(cancelAction)
+        
+        present(ac, animated: true, completion: nil)
+    }
     
 }
--- WeatherApp/View Controllers/ViewController.swift
+++ WeatherApp/View Controllers/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
