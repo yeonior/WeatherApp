e7131ee69966351c40acb9a57ee313a901e29754
Rus <de.mr44@gmail.com>
2021-05-16 19:04:16 +0500

    Added parsing JSON

---
+++ WeatherApp/Models/CurrentWeatherData.swift
@@ -0,0 +1,28 @@
+//
+//  CurrentWeatherData.swift
+//  WeatherApp
+//
+//  Created by ruslan on 16.05.2021.
+//
+
+import Foundation
+
+struct CurrentWeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+}
--- WeatherApp/Models/NetworkWeatherManager.swift
+++ WeatherApp/Models/NetworkWeatherManager.swift
@@ -15,10 +15,19 @@ struct NetworkWeatherManager {
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-                let dataString = String(data: data, encoding: .utf8)
-                print(dataString!)
+                parseJSON(withData: data)
             }
         }
         task.resume()
+    }
+    
+    func parseJSON(withData data: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
+            print(currentWeatherData.main.temp)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
 }
